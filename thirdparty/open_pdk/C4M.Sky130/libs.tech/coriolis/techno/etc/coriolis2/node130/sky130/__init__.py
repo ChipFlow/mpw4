532f1223d1b0f86f0bc1e31c52b3940b4f8776b9
Staf Verhaegen <staf@fibraservi.eu>
2021-12-29 12:10:59 +0100

    [WIP]IOLib

--- thirdparty/open_pdk/C4M.Sky130/libs.tech/coriolis/techno/etc/coriolis2/node130/sky130/__init__.py
+++ thirdparty/open_pdk/C4M.Sky130/libs.tech/coriolis/techno/etc/coriolis2/node130/sky130/__init__.py
@@ -1,4 +1,5 @@
 from .techno import *
 from .StdCellLib import setup as StdCellLib_setup
+from .IOLib import setup as IOLib_setup
 
-__lib_setups__ = [StdCellLib.setup]
+__lib_setups__ = [StdCellLib.setup,IOLib.setup]
